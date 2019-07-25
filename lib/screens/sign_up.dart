2c817c7bbadff30c668ee78ef6bdc8fc22007b2d
masterUNG <phrombutr@gmail.com>
2019-07-25 14:38:39 +0700

    Register Success

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ung_shopping/screens/my_service.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -10,6 +12,7 @@ class _SignUpState extends State<SignUp> {
   double myPadding = 20.0;
   String nameString, emailString, passwordString;
   final formKey = GlobalKey<FormState>();
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   // Method
   Widget showHead() {
@@ -43,6 +46,9 @@ class _SignUpState extends State<SignUp> {
             return 'Please Fill Name in Blank';
           }
         },
+        onSaved: (String value) {
+          nameString = value;
+        },
       ),
     );
   }
@@ -58,10 +64,14 @@ class _SignUpState extends State<SignUp> {
             Icons.email,
             size: 36.0,
           ),
-        ),validator: (String value){
+        ),
+        validator: (String value) {
           if (!((value.contains('@')) && (value.contains('.')))) {
             return 'Please Type Email Format';
           }
+        },
+        onSaved: (String value) {
+          emailString = value;
         },
       ),
     );
@@ -77,10 +87,14 @@ class _SignUpState extends State<SignUp> {
             Icons.lock,
             size: 36.0,
           ),
-        ),validator: (String value){
+        ),
+        validator: (String value) {
           if (value.length < 6) {
             return 'More 6 Charactor';
           }
+        },
+        onSaved: (String value) {
+          passwordString = value;
         },
       ),
     );
@@ -93,9 +107,65 @@ class _SignUpState extends State<SignUp> {
         print('You Click Upload');
         if (formKey.currentState.validate()) {
           formKey.currentState.save();
+          print(
+              'name = $nameString, email = $emailString, password = $passwordString');
+          registerUser();
         }
       },
     );
+  }
+
+  Future<void> registerUser() async {
+    await firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: emailString, password: passwordString)
+        .then((response) {
+      print('Register Success');
+      setupDisplayName();
+    }).catchError((response) {
+      print('Error = ${response.toString()}');
+      String titleString = response.code;
+      String messageString = response.message;
+      myAlert(titleString, messageString);
+    });
+  }
+
+  Future<void> setupDisplayName() async {
+
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    UserUpdateInfo userUpdateInfo = UserUpdateInfo();
+    userUpdateInfo.displayName = nameString;
+    firebaseUser.updateProfile(userUpdateInfo);
+
+    var myServiceRoute = MaterialPageRoute(builder: (BuildContext context) => MyService());
+    Navigator.of(context).pushAndRemoveUntil(myServiceRoute, (Route<dynamic> route) => false);
+
+  }
+
+  void myAlert(String titleString, String messageString) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: ListTile(
+              leading: Icon(
+                Icons.android,
+                size: 48.0,
+                color: Colors.green,
+              ),
+              title: Text(titleString),
+            ),
+            content: Text(messageString),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -106,7 +176,8 @@ class _SignUpState extends State<SignUp> {
         title: Text('Sign Up'),
         actions: <Widget>[uploadButton()],
       ),
-      body: Form(key: formKey,
+      body: Form(
+        key: formKey,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
