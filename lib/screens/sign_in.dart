0153dc7bce7b0ade2e41caa092820212223f8b7f
masterUNG <phrombutr@gmail.com>
2019-07-26 09:50:28 +0700

    update

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -6,13 +6,41 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  // Explicit
+
+  // Method
+  Widget emailText() {
+    return TextFormField(
+      decoration: InputDecoration(hintStyle: TextStyle(color: Colors.red),
+        hintText: 'ABC',
+        labelText: 'Email :',
+        helperText: 'you@email.com',
+      ),
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password :',
+        helperText: 'More 6 Charactor',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign In'),
       ),
-      body: Text('body'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          emailText(),
+          passwordText(),
+        ],
+      ),
     );
   }
 }
